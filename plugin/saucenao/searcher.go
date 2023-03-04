ccfdacd651b6cd3a9be32c6559bb8f5260b25186
Aimer9179 <99615717+Aimer9179@users.noreply.github.com>
2023-03-04 21:55:18 +0800

    Update searcher.go

--- plugin/saucenao/searcher.go
+++ plugin/saucenao/searcher.go
@@ -106,7 +106,7 @@ func init() { // 插件主体
 			}
 		})
 	// 以图搜图
-	engine.OnKeywordGroup([]string{"以图搜图", "搜索图片", "以图识图"}, zero.MustProvidePicture).SetBlock(true).
+	engine.OnKeywordGroup([]string{"以图搜图", "搜图", "以图识图"}, zero.MustProvidePicture).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
 			// 开始搜索图片
 			ctx.SendChain(message.Text("少女祈祷中..."))
